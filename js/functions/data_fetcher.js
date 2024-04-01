15179cf47fceb58a5c2b5c040262bcd6ddb489ba
jorge.torrelles <jorge.torrelles@wundermanthompson.com>
2024-04-01 18:33:24 -0300

    Refactorize code with DOC of API

--- js/functions/data_fetcher.js
+++ js/functions/data_fetcher.js
@@ -1,27 +1,40 @@
 import Config from "../../config.js";
 
 export default class DataFetcher {
-    #Data = {};
+    #Data = [];
     #config = new Config();
 
-    async get_data() {
-        const _base_url_ = "https://api.themoviedb.org/3/movie/";
-        const _options_ = {
+    searchByID(id) {
+        const options = {
             method: 'GET',
             headers: {
                 accept: 'application/json',
                 Authorization: `${this.#config.getToken()}`
             }
         };
+        
+        fetch(`https://api.themoviedb.org/3/collection/${id}?language=en-US&api_key=${this.#config.getApiKey()}`, options)
+            .then(response => response.json())
+            .then(response => this.#Data.push(response))
+            .catch(err => console.error(err));
 
-        let movie_id = "200";
-        let page = "1"
+        return this.#Data;
+    }
 
-        const url = await fetch(`${_base_url_}${movie_id}?page=${page}&api_key=${this.#config.getApiKey()}`, _options_);
-        this.#Data = await url.json();
-
-        console.log(this.#Data);
-
+    searchImgByID(id) {
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: `${this.#config.getToken()}`
+            }
+        };
+        
+        fetch(`https://api.themoviedb.org/3/collection/${id}/images?language=en-US&api_key=${this.#config.getApiKey()}`, options)
+            .then(response => response.json())
+            .then(response => this.#Data.push(response))
+            .catch(err => console.error(err));
+        
         return this.#Data;
     }
 }
--- js/main.js
+++ js/main.js
@@ -3,6 +3,16 @@ import DataFetcher from "./functions/data_fetcher.js";
 document.addEventListener("DOMContentLoaded", async () => {
     const dataFetcher = new DataFetcher();
     const data = await dataFetcher.get_data();
-
-    document.write(`${data}`)
+    
+    if (data && data.title) {
+        document.querySelector("._content").innerHTML = `
+            <span><b>Name/Nombre:</b> ${data.title} / ${data.original_title}</span><br>
+            <span><b>Homepage:</b> ${data.homepage}</span><br>
+            <span><b>Homepage:</b> ${data.homepage}</span><br>
+        `;
+    } else {
+        console.error("No se pudo encontrar el título en los datos.");
+    }
 });
+
+
